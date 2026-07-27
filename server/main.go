79551b63f1985069b05f5db0eb7e363c2a5054fa
Harsh <harshkumarking22@gmail.com>
2026-07-27 22:04:50 +0530

    INIT

---
+++ server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	pb "grpc/protoc"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	list, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to start server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", list.Addr())
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to start grpc server %v", err)
+
+	}
+}
